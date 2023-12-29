98895aff60207364044eb7bb4519cb0649132bc5
jun_ <gurwns232@naver.com>
2023-12-29 11:41:12 +0900

    refactor: tab 간격 수정

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const viewBtn = document.querySelector('.view-modal')
-const popup = document.querySelector('.popup')
-const close = popup.querySelector('.close')
-const field = popup.querySelector('.field')
-const input = popup.querySelector('input')
-const copy = popup.querySelector('button')
+const viewBtn = document.querySelector('.view-modal');
+const popup = document.querySelector('.popup');
+const close = popup.querySelector('.close');
+const field = popup.querySelector('.field');
+const input = popup.querySelector('input');
+const copy = popup.querySelector('button');
 
 viewBtn.addEventListener('click', () => {
-    popup.classList.toggle('show')
-})
+  popup.classList.toggle('show');
+});
 
 close.addEventListener('click', () => {
-    viewBtn.click()
-})
+  viewBtn.click();
+});
 
 copy.addEventListener('click', async () => {
-    input.select()
+  input.select();
 
-    navigator.clipboard
-        .writeText(input.value)
-        .then(() => {
-            field.classList.add('acitve')
-            copy.innerText = 'Copied'
-            setTimeout(() => {
-                field.classList.remove('active')
-                copy.innerText = 'Copy'
-                window.getSelection().removeAllRanges()
-            }, 3000)
-        })
-        .catch(err => {
-            console.log('Something went wrong', err)
-        })
-})
+  navigator.clipboard
+    .writeText(input.value)
+    .then(() => {
+      field.classList.add('acitve');
+      copy.innerText = 'Copied';
+      setTimeout(() => {
+        field.classList.remove('active');
+        copy.innerText = 'Copy';
+        window.getSelection().removeAllRanges();
+      }, 3000);
+    })
+    .catch((err) => {
+      console.log('Something went wrong', err);
+    });
+});
